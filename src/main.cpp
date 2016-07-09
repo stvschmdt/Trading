be0a451eb06fd19c808a28688297f4a306b1560d
steve schmidt <quantdata@gmail.com>
2016-07-08 19:17:56 -0700

    initial cpp files/dirs

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<sqlite3.h>
+
+/*master todo
+
+create master c++ header file [main.h]
+create argument parser [main.cpp]
+creater parameter file reader [main.cpp]
+
+
+*/
+
+int main(int argc, char ** argv){
+	std::cout<<"beginning Trading simulation"<<std::endl;
+	return 0;
+}
